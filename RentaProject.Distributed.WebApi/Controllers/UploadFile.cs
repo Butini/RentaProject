c7a2b8176ea18174f995979f5980e990f680b16c
Butini <marcbiedmabutier@gmail.com>
2021-07-25 11:45:15 +0200

    Try upload controller. #6

---
+++ RentaProject.Distributed.WebApi/Controllers/UploadFile.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentaProject.Distributed.WebApi.Controllers
+{
+    public class UploadFile : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("upload")]
+        public string Upload(string s)
+        {
+            return s;
+        }
+    }
+}
