c98bbf12e470fffb41276352a6d337513eebb12c
Butini <marcbiedmabutier@gmail.com>
2021-07-25 12:58:32 +0200

    Add Action and Bitcoin entities. #2

---
+++ RentaProject.Domain.Entities/Bitcon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentaProject.Domain.Entities
+{
+    public class Bitcon : Action
+    {
+        public decimal OpenRate { get; set; }
+        public decimal CloseRate { get; set; }
+        public DateTime OpenDate { get; set; }
+        public DateTime CloseDate { get; set; }
+
+        public Bitcon(string name, decimal profit, decimal fees, 
+            decimal openRate, decimal closeRate, DateTime openDate, DateTime closeDate) : base(name, profit, fees)
+        {
+            OpenRate = openRate;
+            CloseRate = closeRate;
+            OpenDate = openDate;
+            CloseDate = closeDate;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Name, Profit, Fees, OpenRate, CloseRate, OpenDate, CloseDate);
+        }
+    }
+}
--- RentaProject.Domain.Entities/FileManager.cs
+++ RentaProject.Domain.Entities/FileManager.cs
@@ -8,5 +8,17 @@ namespace RentaProject.Domain.Entities
 {
     public class FileManager
     {
+        public string MyFile { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is FileManager manager &&
+                   MyFile == manager.MyFile;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(MyFile);
+        }
     }
 }
